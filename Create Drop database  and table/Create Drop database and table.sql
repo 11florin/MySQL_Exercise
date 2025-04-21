dabc4ea58c3655b7679d419c776c621ba7cbbd3f
Florin Albu <albuflorin929@yahoo.co.uk>
2025-04-21 13:25:31 +0100

    Create & Drop Database & Table

---
+++ Create Drop database  and table/Create Drop database and table.sql
@@ -0,0 +1,43 @@
+-- Create Database
+CREATE DATABASE soap_store;
+
+-- Drop databse
+DROP DATABASE databse_name;
+
+-- To use database
+USE databse_name;
+
+-- Creating Tables:
+CREATE TABLE cats (
+name VARCHAR(50),
+age INT
+); 
+ 
+CREATE TABLE dogs (
+name VARCHAR(50),
+breed VARCHAR(50),
+age INT
+);
+
+-- Drop a table
+DROP TABLE cats;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
