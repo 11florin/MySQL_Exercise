f49958a1f994e7650a7ce8f5d115450f39ef3ba6
Florin Albu <albuflorin929@yahoo.co.uk>
2025-04-22 10:53:42 +0100

    String Function

---
+++ SUBSTRING/SUBSTRING.sql
@@ -0,0 +1,20 @@
+-- SUBSTRING
+
+SELECT SUBSTRING('Hello World', 1, 4);
+ 
+SELECT SUBSTRING('Hello World', 7);
+ 
+SELECT SUBSTRING('Hello World', -3);
+ 
+SELECT SUBSTRING(title, 1, 10) AS 'short title' FROM books;
+ 
+SELECT SUBSTR(title, 1, 10) AS 'short title' FROM books;
+
+-- Combining String function 
+
+SELECT CONCAT
+    (
+        SUBSTRING(title, 1, 10),
+        '...'
+    ) AS 'short title'
+FROM books;
